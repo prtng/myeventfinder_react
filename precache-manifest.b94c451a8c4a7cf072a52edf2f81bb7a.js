640651f48e035aa9c567c0266d86d58a92e481eb
prtng <prachitonngaonkar@hotmail.com>
2020-07-12 13:07:03 +0530

    Updates

---
+++ precache-manifest.b94c451a8c4a7cf072a52edf2f81bb7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43df980e3fae7940faf1e8c2e138dceb",
+    "url": "/myeventfinder_react/index.html"
+  },
+  {
+    "revision": "b751c60ad668657f4a53",
+    "url": "/myeventfinder_react/static/css/2.f8236e86.chunk.css"
+  },
+  {
+    "revision": "4779955ccffd3653aa54",
+    "url": "/myeventfinder_react/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "b751c60ad668657f4a53",
+    "url": "/myeventfinder_react/static/js/2.47ef4817.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myeventfinder_react/static/js/2.47ef4817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4779955ccffd3653aa54",
+    "url": "/myeventfinder_react/static/js/main.a1b5b21a.chunk.js"
+  },
+  {
+    "revision": "9a9c96ec5a2347c4b269",
+    "url": "/myeventfinder_react/static/js/runtime-main.f498a88a.js"
+  }
+]);
